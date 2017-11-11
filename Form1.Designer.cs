953d676c9615f74245f935872c78ba8c73045b7d
paulinalebiedzinska <32547589+paulinalebiedzinska@users.noreply.github.com>
2017-11-11 19:10:31 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PSprawdzania = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Location = new System.Drawing.Point(48, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "__________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wprowadź literę:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Location = new System.Drawing.Point(116, 77);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(16, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // PSprawdzania
+            // 
+            this.PSprawdzania.Location = new System.Drawing.Point(148, 77);
+            this.PSprawdzania.Name = "PSprawdzania";
+            this.PSprawdzania.Size = new System.Drawing.Size(75, 23);
+            this.PSprawdzania.TabIndex = 3;
+            this.PSprawdzania.Text = "Sprawdź";
+            this.PSprawdzania.UseVisualStyleBackColor = true;
+            this.PSprawdzania.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "To będzie twoja 1 próba.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Nowe słowo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Zamknij";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PSprawdzania);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Wisielec";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button PSprawdzania;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string NaszWyraz = null;
+        string ZgadywanyWyraz = null;
+        string ZgadywanyWyrazSpacje = null;
+
+        int KrokZgadywania = 1;
+        string literka;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            NaszWyraz = "KABARET";
+            ZgadywanyWyraz = "_______";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                literka = textBox1.Text;
+                for (int i = 0; i< NaszWyraz.Length; i++)
+                {
+                    if(literka == NaszWyraz.Substring(i,1))
+                    {
+                        ZgadywanyWyraz = ZgadywanyWyraz.Remove(i,1);
+                        ZgadywanyWyraz = ZgadywanyWyraz.Insert(i, literka);
+                        ZgadywanyWyrazSpacje = ZgadywanyWyraz;
+                        for(int j = ZgadywanyWyraz.Length-1; j >= 0 ; j--)
+                        {
+                            ZgadywanyWyrazSpacje = ZgadywanyWyrazSpacje.Insert(j, " ");
+                        }
+
+                        label1.Text = ZgadywanyWyrazSpacje;
+                    }
+                }
+            }
+           
+            textBox1.Text = "";
+            KrokZgadywania++;
+            if(literka != "")
+                label3.Text = "To będzie Twoja " + KrokZgadywania + " próba.";
+            if(!ZgadywanyWyraz.Contains("_"))
+            {
+                label3.Text = "Gratulacje. Udało ci się w " + (KrokZgadywania - 1) + " próbach!";
+                PSprawdzania.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //PSprawdzania.PerformClick();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
